4579437f109d2930261add2ae34468fcf4c6a14c
Chen Song <songminmin999@gmail.com>
2017-04-05 22:37:19 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,35 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group= @group
+    @post.user = current_user
+
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group =@group
+    @post.user = current_user
+    if @post.update(post_params)
+      redirect_to posts_account_path, notice: "编辑成功！"
+    else
+      render :edit
+    end
+  end
+
+    def delete
+      @group = Group.find([:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.user = current_user
+
+      @post.destroy
+        rediret_to posts_account_path, warning: "删除成功！"
+    end
+
 end
